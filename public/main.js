30e11573fc3d0d36e1c9edf06f131988e10b4a6f
Juan <juanxsalazar21@gmail.com>
2019-02-12 16:44:51 -0500

    console log of weather is working

--- public/main.js
+++ public/main.js
@@ -1,7 +1,27 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const getWeather = () => {
+  // function for getting weather
+  const searchTerm = document.querySelector('.search-term').value
+  //  ^ allows the search input text to specify the place we want to see weather of
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=4874272fe78ea13697eb80a62b2b86e9&units=imperial`
+//  ^ api url where we will get the weather.  $ {} is the string interpolation where the directory will happen. 
+  console.log(url)
+  // ^ console.log (checks to see if it actual works on inspect console)
+fetch ('https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=4874272fe78ea13697eb80a62b2b86e9&units=imperial')
+//  ^ this is the promise that lets us fetch the url
+.then(response => response.json())
+
+
+  // make get request to weather api
+  // parse results
+  // add current weather to html
 }
 
-document.addEventListener('DOMContentLoaded', main)
+
+
+
+
+document.querySelector('.search').addEventListener('click', getWeather)
+// ^ allows button to work 
+
+// * https://api.openweathermap.org/data/2.5/weather?q=&appid=4874272fe78ea13697eb80a62b2b86e9
+//  &units=imperial makes the degrees in farenheight 

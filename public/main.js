db70432aa31f4de01b97334fb6d7e35ddf1d2737
Juan <juanxsalazar21@gmail.com>
2019-02-12 23:08:59 -0500

    weather searcher works

--- public/main.js
+++ public/main.js
@@ -1,27 +1,24 @@
 const getWeather = () => {
-  // function for getting weather
   const searchTerm = document.querySelector('.search-term').value
-  //  ^ allows the search input text to specify the place we want to see weather of
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=4874272fe78ea13697eb80a62b2b86e9&units=imperial`
-//  ^ api url where we will get the weather.  $ {} is the string interpolation where the directory will happen. 
   console.log(url)
-  // ^ console.log (checks to see if it actual works on inspect console)
-fetch ('https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=4874272fe78ea13697eb80a62b2b86e9&units=imperial')
-//  ^ this is the promise that lets us fetch the url
-.then(response => response.json())
-
-
-  // make get request to weather api
-  // parse results
-  // add current weather to html
+  fetch (url) 
+  .then(resp => {
+    return resp.json()
+  })
+  .then (weatherData => {
+    currentWeather = weatherData
+    document.querySelector('.weather-type').textContent = 'Current Weather:' + weatherData.weather[0].main
+    document.querySelector('.location').textContent = weatherData.name + ', ' + weatherData.sys.country
+    document.querySelector('.temp').textContent = 'Current Temperature:' + weatherData.main.temp + '°F'
+    document.querySelector('.min-temp').textContent = 'Min Temperature:' + weatherData.main.temp_min + '°F'
+    document.querySelector('.max-temp').textContent = 'Max Temperature:' + weatherData.main.temp_max + '°F'
+  })
 }
 
 
-
-
-
 document.querySelector('.search').addEventListener('click', getWeather)
-// ^ allows button to work 
+
 
 // * https://api.openweathermap.org/data/2.5/weather?q=&appid=4874272fe78ea13697eb80a62b2b86e9
-//  &units=imperial makes the degrees in farenheight 
+//  &units=imperial makes the degrees 

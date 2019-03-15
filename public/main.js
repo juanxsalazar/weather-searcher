34430fd47c03bcbd9c8343799e70a00e346dc08a
Juan <juanxsalazar21@gmail.com>
2019-03-15 14:22:02 -0400

    added date and time clock

--- public/main.js
+++ public/main.js
@@ -1,11 +1,37 @@
+let myHour = document.getElementById('hour')
+let myMinutes = document.getElementById('minutes')
+let mySecond = document.getElementById('second')
+let presentDate = document.getElementById('day')
+
+const myClock = () => {
+  let now = new Date()
+  let hour = now.getHours()
+  let minutes = now.getMinutes()
+  let second = now.getSeconds()
+
+  if (second < 10) { second = '0' + second }
+  if (minutes < 10) { minutes = '0' + minutes }
+  if (hour < 10) { hour = '0' + hour }
+  let currentDate = now.toDateString()
+
+  presentDate.textContent = currentDate
+  myHour.textContent = hour + ':'
+  myMinutes.textContent = minutes + ':'
+  mySecond.textContent = second
+}
+
+setInterval(myClock, 500)
+
+
+
 const getWeather = () => {
   const searchTerm = document.querySelector('.search-term').value
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=4874272fe78ea13697eb80a62b2b86e9&units=imperial`
   console.log(url)
   fetch (url) 
   .then(resp => {
-    return resp.json()
-  })
+      return resp.json()
+    })
   .then (weatherData => {
     currentWeather = weatherData
     document.querySelector('.weather-type').textContent = 'Current Weather:' + weatherData.weather[0].main
